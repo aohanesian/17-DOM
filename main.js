a35e03c84515589b8b8bb9ec31dd8083931a73a9
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-10-18 04:47:08 +0300

    comm

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 let lastLis = document.querySelectorAll(`ul > li:last-of-type`);
 lastLis.forEach(li => li.classList.add(`last`));
 
-function setFirstItemClassName(level) {
-    let deep = ``;
-    for (let i = 1; i < level; i++) {
-        deep += `> li > ul`
+const ulRoot = document.querySelector(`ul.root`);
+
+function setFirstItemClassName(level, ul = ulRoot) {
+    level--;
+    if (level > 0) {
+        const LIs = [...ul.children];
+        LIs.forEach(LI => {
+            let InnerULs = [...LI.children];
+            InnerULs.forEach(InnerUL => setFirstItemClassName(level, InnerUL))
+        });
+    } else {
+        let LiFirst = ul.querySelector(`li:first-of-type`);
+        LiFirst.classList.add(`first`);
     }
-    let lis = document.querySelectorAll(`ul.root ${deep} > li:first-child`);
-    lis.forEach(li => li.classList.add('first'));
 }
 
 setFirstItemClassName(2);
+
+// function setFirstItemClassName(level) {
+//     let deep = ``;
+//     for (let i = 1; i < level; i++) {
+//         deep += `> li > ul`
+//     }
+//     let lis = document.querySelectorAll(`ul.root ${deep} > li:first-child`);
+//     lis.forEach(li => li.classList.add('first'));
+// }
